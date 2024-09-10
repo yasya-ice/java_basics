b33649c7f74159974eb30b5f4a75693fbb6c8838
Yaroslava Nahorna <yasya74800@gmail.com>
2024-09-10 10:44:04 +0300

    ex 14

---
+++ src/positive_or_not.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class positive_or_not {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type a number: ");
+        double number = scanner.nextDouble();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } if (number ==0) {
+            System.out.println("This is neither positive nor negative.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+        scanner.close();
+    }
+}
