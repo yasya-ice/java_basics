119d205ea30ac3419c9ec30edf46fe3744165a66
Yaroslava Nahorna <yasya74800@gmail.com>
2024-09-10 14:47:29 +0300

    ex 29

--- src/week2/even_numbers.java
+++ src/week2/even_numbers.java
@@ -1,2 +1,9 @@
+package week2;
+
 public class even_numbers {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i+=2 ) {
+            System.out.println(i);
+        }
+    }
 }
