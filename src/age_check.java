daf80cc880d168aaf6e553ac351d187fd778cda9
Yaroslava Nahorna <yasya74800@gmail.com>
2024-09-10 11:38:45 +0300

    ex 19

---
+++ src/age_check.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class age_check {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How old are you? ");
+        int age = scanner.nextInt();
+
+        if (age >= 0 && age <= 120) {
+            System.out.println("OK ");
+        } else if (age <= 0) {
+            System.out.println("Impossible! ");
+        }
+    }
+}
