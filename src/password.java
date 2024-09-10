8e74b94e232a997acab75bfd6f7566a1a0a3435b
Yaroslava Nahorna <yasya74800@gmail.com>
2024-09-10 13:36:09 +0300

    ex 22.2

--- src/password.java
+++ src/password.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class password {
     public static void main(String[] args) {
-
         String correctPassword = "qwerty";
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("Type the password: ");
-        String enteredPassword = sc.nextLine();
 
-        if (enteredPassword.equals(correctPassword)) {
-            System.out.println("Right!");
-        } else {
-            System.out.println("Wrong!");
+        while (true) {
+            System.out.print("Type the password: ");
+            String enteredPassword = sc.nextLine();
+
+            if (enteredPassword.equals(correctPassword)) {
+                System.out.println("Right!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
         }
 
         sc.close();
