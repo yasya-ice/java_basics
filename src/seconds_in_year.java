f71098c6df7b493039def968ea3705211caf7b9d
Yaroslava Nahorna <yasya74800@gmail.com>
2024-09-10 09:42:44 +0300

    ex 5

---
+++ src/seconds_in_year.java
@@ -0,0 +1,11 @@
+public class seconds_in_year {
+    public static void main(String[] args) {
+        int SecondsInMinutes= 60;
+        int MinutesInHours= 60;
+        int HoursInDays= 24;
+        int DaysInYear= 365;
+        int SecondsInYear;
+        SecondsInYear = SecondsInMinutes * MinutesInHours * HoursInDays * DaysInYear;
+        System.out.println("There are " + SecondsInYear + " seconds in year.");
+    }
+}
