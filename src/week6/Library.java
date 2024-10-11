a839bff363c6f2f007dcf481df9ceebb9f0a667b
Yaroslava Nahorna <yasya74800@gmail.com>
2024-10-11 09:44:05 +0300

    Ex 101.3 Library: search functionality

--- src/week6/Library.java
+++ src/week6/Library.java
@@ -18,18 +18,59 @@ public class Library {
         }
     }
 
-    public static void main(String[] args) {
+    public ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> found = new ArrayList<>();
+        for (Book book : this.books) {
+            if (book.title().toLowerCase().contains(title.toLowerCase())) {
+                found.add(book);
+            }
+        }
+        return found;
+    }
 
+    public ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> found = new ArrayList<>();
+        for (Book book : this.books) {
+            if (book.publisher().toLowerCase().contains(publisher.toLowerCase())) {
+                found.add(book);
+            }
+        }
+        return found;
+    }
+
+    public ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> found = new ArrayList<>();
+        for (Book book : this.books) {
+            if (book.year() == year) {
+                found.add(book);
+            }
+        }
+        return found;
+    }
+
+    public static void main(String[] args) {
         Library library = new Library();
 
-        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
-        library.addBook(cheese);
-
-        Book nhl = new Book("NHL Hockey", "Stanley Kupp", 1952);
-        library.addBook(nhl);
-
+        library.addBook(new Book("Cheese Problems Solved", "Woodhead Publishing", 2007));
+        library.addBook(new Book("The Stinky Cheese Man and Other Fairly Stupid Tales", "Penguin Group", 1992));
+        library.addBook(new Book("NHL Hockey", "Stanley Kupp", 1952));
         library.addBook(new Book("Battle Axes", "Tom A. Hawk", 1851));
 
-        library.printBooks();
+        ArrayList<Book> result = library.searchByTitle("Cheese");
+        for (Book book : result) {
+            System.out.println(book);
+        }
+
+        System.out.println("---");
+
+        for (Book book : library.searchByPublisher("Penguin Group")) {
+            System.out.println(book);
+        }
+
+        System.out.println("---");
+
+        for (Book book : library.searchByYear(1851)) {
+            System.out.println(book);
+        }
     }
 }
