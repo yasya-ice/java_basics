55329e783e7b9a84e8e6b8e3de1f470ca39498be
Yaroslava Nahorna <yasya74800@gmail.com>
2024-10-11 09:15:06 +0300

    Ex 97

---
+++ src/week6/Elegantly.java
@@ -0,0 +1,14 @@
+package week6;
+
+public class Elegantly {
+        public static void main(String[] args) {
+            int [] array = {5, 1,3, 4,2};
+            printElegantly(array);
+        }
+        public static void printElegantly(int [] array) {
+            for (int i = 0; i < array.length - 1; i++) {
+                System.out.print(array[i] + ", ");
+            }
+            System.out.println(array[array.length - 1]);
+        }
+    }
