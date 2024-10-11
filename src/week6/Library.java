1476e20ff74680eca77354d43093602d3d8a18bf
Yaroslava Nahorna <yasya74800@gmail.com>
2024-10-11 09:40:30 +0300

    Ex 101.2 Library

--- src/week6/Library.java
+++ src/week6/Library.java
@@ -1,4 +1,35 @@
 package week6;
+import java.util.ArrayList;
 
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book newBook) {
+        this.books.add(newBook);
+    }
+
+    public void printBooks() {
+        for (Book book : this.books) {
+            System.out.println(book);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Library library = new Library();
+
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        library.addBook(cheese);
+
+        Book nhl = new Book("NHL Hockey", "Stanley Kupp", 1952);
+        library.addBook(nhl);
+
+        library.addBook(new Book("Battle Axes", "Tom A. Hawk", 1851));
+
+        library.printBooks();
+    }
 }
